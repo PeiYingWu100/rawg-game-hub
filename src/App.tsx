25cf3c6ee43f3a385020028a0ea1510ea171cf7b
Pei-Ying Wu <peiying.wu100@gmail.com>
2023-12-08 12:04:18 +1100

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav"
+              "main"`,
+        lg: `"nav nav"
+            "sidebar main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg="pink">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"sidebar"} bg="green">
+          SideBar
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
