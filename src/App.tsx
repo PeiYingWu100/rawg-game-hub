dd2b671ef86e255f188ac7a531f11b42acb748d4
Pei-Ying Wu <peiying.wu100@gmail.com>
2023-12-08 12:57:00 +1100

    Build a nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       }}
     >
       <GridItem area={"nav"} bg="pink">
-        Nav
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area={"sidebar"} bg="green">
