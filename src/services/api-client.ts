af883f590e38a8567b34b734781c0c2627a25a0e
Pei-Ying Wu <peiying.wu100@gmail.com>
2023-12-12 15:10:18 +1100

    Create a custom hook to fetch the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api", 
     params: {
-        // 2023/12/12
+        // 2023/12/12, temporary solution
         key: "0afe64f47228433583effd0fa49c4761"
     }
 })
