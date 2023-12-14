5504cda7dabcb2887bdf4ab655be923cb00084ed
Pei-Ying Wu <peiying.wu100@gmail.com>
2023-12-14 18:21:32 +1100

    Display critic score & Get optimised images

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Prop {
+  score: number;
+}
+
+const CriticScore = ({ score }: Prop) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+
+  return (
+    <Badge
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+      colorScheme={color}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
